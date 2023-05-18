2bc10e85699838d4c4bc0f3b3fc0b9ea10c680e5
Robbe Bierebeeck <robbebierebeeck@Robbes-MacBook-Pro-6.local>
2023-05-19 00:16:58 +0200

    feat: fetch test data

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
-const express = require('express');
+import express from 'express';
 const app = express();
+import fetch from 'node-fetch';
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
+
+const fetchData =async () => {
+
+    const response = await fetch('https://api.github.com/users/github');
+    const data = await response.json();
+
+}
+
+fetchData()
+
+//fetch the data every 5 seconds
+setInterval(fetchData, 5000);
+
 
 const server = app.listen(3000, function () {
     var host = server.address().address;
