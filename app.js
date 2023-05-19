41ebc12875495f48766a0199dac6beb0ad53636c
Robbe Bierebeeck <robbebierebeeck@Robbes-MacBook-Pro-6.local>
2023-05-19 22:12:30 +0200

    feat: fetch data on internval

--- app.js
+++ app.js
@@ -1,19 +1,46 @@
-import express from 'express';
+const express = require('express');
+const gpio = require('rpi-gpio');
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 const app = express();
-import fetch from 'node-fetch';
+/*
+// Define the port to run on
+gpio.setup(12, gpio.DIR_OUT, false);
+
+// the possible states of the pin
+const on = () => { gpio.write(12, true, function(err) { if (err) throw err; console.log('Written to pin'); }); }
+
+const off = ()=> { gpio.write(12, false, function(err) { if (err) throw err; console.log('Written to pin'); }); }
+
+*/
+const poorCoffee = () => {
+    console.log('poor coffee')
+
+    timeout()
+}
+
+// set timeout that can be triggered by the noCoffee function
+const timeout = () => {
+   //stop the setIntervall for 2 minutes
+    clearInterval(interval)
+    // set the timeout for 2 minutes
+    setTimeout(() => {
+        // restart the interval
+        interval = setInterval(fetchData, 5000)
+    }   , 120000)
+
+}
 
 
-const fetchData =async () => {
-
-    const response = await fetch('https://api.github.com/users/github');
+const fetchData = async () => {
+    const response = await fetch('https://globalclassroom.bierebeeck.be/status.php');
     const data = await response.json();
-
+    data.status === true ? poorCoffee() : console.log('no coffee')
 }
 
 fetchData()
 
 //fetch the data every 5 seconds
-setInterval(fetchData, 5000);
+let interval = setInterval(fetchData, 5000)
 
 
 const server = app.listen(3000, function () {
